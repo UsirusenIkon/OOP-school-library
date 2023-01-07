4b3ee1c7d291f7b41677af9980947abbe0751882
Usirusen Ikon <lexicon048@gmail.com>
2023-01-07 13:59:20 +0100

    Introduced bug

--- book.rb
+++ book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :title, :author
   attr_reader :rentals
 
-  def initialize(title, author)
+  def intialize(title, author)
     @title = title
     @author = author
     @rentals = []
